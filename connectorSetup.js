42b717bfc70a30aff0a9019e4ade3e7e1a83c83a
threebrick <darnell@threebrick.com>
2017-06-16 14:57:09 -0400

    test 4

--- connectorSetup.js
+++ connectorSetup.js
@@ -4,8 +4,8 @@ module.exports = function() {
     global.builder = require('botbuilder');
 
     var connector = new builder.ChatConnector({
-            appId: process.env.MICROSOFT_APP_ID ? process.env.MICROSOFT_APP_ID : '2ada3b5a-69f4-43c6-9ac0-0ddd1c181f63',
-            appPassword: process.env.MICROSOFT_APP_PASSWORD ? process.env.MICROSOFT_APP_PASSWORD : '1Vn1uwsvpZqH6oQg4iTBW0V',
+            appId: '2ada3b5a-69f4-43c6-9ac0-0ddd1c181f63',
+            appPassword:  '1Vn1uwsvpZqH6oQg4iTBW0V',
            gzipData: true
         });
 
